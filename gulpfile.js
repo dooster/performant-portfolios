813264eaac817b8b5d3a118cbc6a27cafebe9a9d
dooster <jisbender@gmail.com>
2016-01-27 17:43:52 -0500

    Add git ignore

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+//File modified from tutorial at CSS-Tricks
+//https://css-tricks.com/gulp-for-beginners/
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('', function () {
+
+
+})
